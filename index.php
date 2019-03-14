eca1af1fbdddaa5314121278b30fdaafaa9dc271
gabrielmodesto <gabriel.modesto@nexttecnologiadainformacao.com.br>
2019-03-14 16:05:21 -0300

    Adicionados metodos de delete e put

--- index.php
+++ index.php
@@ -10,8 +10,21 @@ $app = new \Slim\App();
 $app->post('/', function(Request $request, Response $response, array $args){
     $data = $request->getParsedBody();
     $nome = $data['nome'] ?? '';
-    return $response->getBody()->write("Produto {$nome}");
+    return $response->getBody()->write("Produto {$nome} (POST)");
+    
+});
+$app->put('/', function(Request $request, Response $response, array $args){ 
+    $data = $request->getParsedBody();
+    $nome = $data['nome'] ?? '';
+    return $response->getBody()->write("Produto {$nome} (PUT)");
+});
+
+$app->delete('/', function(Request $request, Response $response, array $args){
+    $data = $request->getParsedBody();
+    $nome = $data['nome'] ?? '';
+    return $response->getBody()->write("Produto {$nome} (DELETE)");
     
 });
 
 $app->run();
+    
