9c951b616aa5c4474e40843489419f2ed7eb5d34
gabrielmodesto <gabriel.modesto@nexttecnologiadainformacao.com.br>
2019-03-15 12:36:37 -0300

    Configurando e organizando a arquitetura do software

--- index.php
+++ index.php
@@ -1,34 +1,5 @@
 <?php
 
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Psr\Http\Message\ResponseInterface as Response;
+require './vendor/autoload.php';
+require './env.php';
 
-require_once 'vendor/autoload.php';
-
-$configuration = [
-    'settings' => [
-        'displayErrorDetails' => true,
-    ],
-];
-$configuration = new \Slim\Container($configuration);
-
-//middleware
-$mid01 = function(Request $request, Response $response, $next): Response{
-    $response->getBody()->write("Middleware 1<br>");
-    $response = $next($request, $response);
-    $response->getBody()->write("<br>Middleware 2");
-
-    return $response;
-};
-
-$app = new \Slim\App($configuration);
-
-$app->post('/', function(Request $request, Response $response, array $args): Response{
-    $data = $request->getParsedBody();
-    $nome = $data['nome'] ?? '';
-    $response->getBody()->write("Produto {$nome} (POST)");
-    return $response;
-})->add($mid01);
-
-$app->run();
-    
