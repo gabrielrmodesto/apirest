f5582b6b10ac5acc78925093feb5d3c996ab7703
gabrielmodesto <gabriel.modesto@nexttecnologiadainformacao.com.br>
2019-03-21 10:32:12 -0300

    Rota concatenada de forma correta

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
 require './vendor/autoload.php';
-require './.env.php';
+require './env.php';
 require './src/slimConfiguration.php';
 require './routes/routes.php';
--- routes/routes.php
+++ routes/routes.php
@@ -8,15 +8,15 @@ $app = new \Slim\App(slimConfiguration());
 
 //rotas para o crud do projeto
 //4 rotas para a loja, criar, ler, alterar e deletar loja
-$app->get('/', LojaController::class, ':getLojas'); //le a loja
-$app->post('/loja', LojaController::class, ':insertLoja'); //cria a loja
-$app->put('/loja', LojaController::class, ':updateLoja'); //altera a loja
-$app->delete('/loja', LojaController::class, ':deleteLoja'); //deleta a loja
+$app->get('/loja', LojaController::class . ':getLojas'); //le a loja
+$app->post('/loja', LojaController::class . ':insertLoja'); //cria a loja
+$app->put('/loja', LojaController::class . ':updateLoja'); //altera a loja
+$app->delete('/loja', LojaController::class . ':deleteLoja'); //deleta a loja
 
 //4 rotas para a produto, criar, ler, alterar e deletar produto
-$app->get('/produto', ProductController::class, ':getProdutos'); //le a produto
-$app->post('/produto', ProductController::class, ':insertProduto'); //cria a produto
-$app->put('/produto', ProductController::class, ':updateProduto'); //altera a produto
-$app->delete('/produto', ProductController::class, ':deleteProduto'); //deleta a produto
+$app->get('/produto', ProductController::class . ':getProdutos'); //le a produto
+$app->post('/produto', ProductController::class . ':insertProduto'); //cria a produto
+$app->put('/produto', ProductController::class . ':updateProduto'); //altera a produto
+$app->delete('/produto', ProductController::class . ':deleteProduto'); //deleta a produto
 
 $app->run();
