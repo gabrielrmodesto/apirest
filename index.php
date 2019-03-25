44e2abc4dc88f5c8e1253be52a1b87c28fd07028
gabrielmodesto <gabriel.modesto@nexttecnologiadainformacao.com.br>
2019-03-24 23:11:45 -0300

    basic auth para autenticar user

--- index.php
+++ index.php
@@ -3,4 +3,5 @@
 require './vendor/autoload.php';
 require './env.php';
 require './src/slimConfiguration.php';
+require './src/basicAuth.php';
 require './routes/routes.php';
--- routes/routes.php
+++ routes/routes.php
@@ -3,20 +3,23 @@
 use function src\slimConfiguration;
 use App\Controller\ProductController;
 use App\Controller\LojaController;
+use function src\basicAuth;
 
 $app = new \Slim\App(slimConfiguration());
 
-//rotas para o crud do projeto
-//4 rotas para a loja, criar, ler, alterar e deletar loja
-$app->get('/loja', LojaController::class . ':getLojas'); //le a loja
-$app->post('/loja', LojaController::class . ':insertLoja'); //cria a loja
-$app->put('/loja', LojaController::class . ':updateLoja'); //altera a loja
-$app->delete('/loja', LojaController::class . ':deleteLoja'); //deleta a loja
+//agroupar rotas
+$app->group('', function() use ($app){
+    //rotas para o crud do projeto
+    //4 rotas para a loja, criar, ler, alterar e deletar loja
+    $app->get('/loja', LojaController::class . ':getLojas'); //le a loja
+    $app->post('/loja', LojaController::class . ':insertLoja'); //cria a loja
+    $app->put('/loja', LojaController::class . ':updateLoja'); //altera a loja
+    $app->delete('/loja', LojaController::class . ':deleteLoja'); //deleta a loja
 
-//4 rotas para a produto, criar, ler, alterar e deletar produto
-$app->get('/produto', ProductController::class . ':getProdutos'); //le a produto
-$app->post('/produto', ProductController::class . ':insertProduto'); //cria a produto
-$app->put('/produto', ProductController::class . ':updateProduto'); //altera a produto
-$app->delete('/produto', ProductController::class . ':deleteProduto'); //deleta a produto
-
+    //4 rotas para a produto, criar, ler, alterar e deletar produto
+    $app->get('/produto', ProductController::class . ':getProdutos'); //le a produto
+    $app->post('/produto', ProductController::class . ':insertProduto'); //cria a produto
+    $app->put('/produto', ProductController::class . ':updateProduto'); //altera a produto
+    $app->delete('/produto', ProductController::class . ':deleteProduto'); //deleta a produto
+})->add(basicAuth());
 $app->run();
