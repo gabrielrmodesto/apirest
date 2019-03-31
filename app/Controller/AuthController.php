f42a6e3b44a3d34455f14eb34cdf091cf0eaf395
gabrielmodesto <gabriel.modesto@nexttecnologiadainformacao.com.br>
2019-03-31 20:30:05 -0300

    Verificando se email e senha estão corretos

--- app/Controller/AuthController.php
+++ app/Controller/AuthController.php
@@ -7,12 +7,20 @@ use Psr\Http\Message\ResponseInterface as Response;
 use App\DAO\GerenciadorDeLojas\UsuariosDAO;
 
 final class AuthController{
-    public function login(Request $request, Response $response, array $args){
+    public function login(Request $request, Response $response, array $args): Response{
         $data = $request->getParsedBody();
 
-        $email = $data['email'];
+        $email = $data['email']; 
+        $senha = $data['senha'];
         $usuarioDAO = new UsuariosDAO();
         $usuario = $usuarioDAO->getUserByEmail($email);
+
+        if(is_null($usuario))
+            return $response->withStatus(401);
+
+        if(password_verify($senha, $usuario->getSenha()))
+            return $response->withStatus(401);
+
         var_dump($usuario);
 
         return $response;
--- app/DAO/GerenciadorDeLojas/UsuariosDAO.php
+++ app/DAO/GerenciadorDeLojas/UsuariosDAO.php
@@ -9,22 +9,23 @@ class UsuariosDAO extends Conexao{
     public function __construct(){
         parent::__construct();   
     }
-    public function getUserByEmail(string $email): ?UsuarioModel{
+    public function getUserByEmail(string $email): ? UsuarioModel{
         $statement = $this->pdo
                           ->prepare('SELECT * FROM usuarios where email= :email');
         $statement->bindParam('email', $email);
         $statement->execute();
         $usuarios = $statement->fetchAll(\PDO::FETCH_ASSOC);
         
-        if($usuarios->rowCount() > 0){
-            $usuario = new UsuarioModel();
-            $usuario->setId($usuario[0]['id'])
-                    ->setNome($usuario[0]['nome'])
-                    ->setEmail($usuario[0]['email'])
-                    ->setSenha($usuario[0]['senha']);
-            return $usuario;
+        if(count($usuarios) === 0){ 
+            return null;
         }
+        $usuario = new UsuarioModel();
+        $usuario->setId($usuarios[0]['id'])
+            ->setNome($usuarios[0]['nome'])
+            ->setEmail($usuarios[0]['email'])
+            ->setSenha($usuarios[0]['senha']);
+        return $usuario;
         
-        return null;
+        
     }
 }
